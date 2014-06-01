276ea346c8f433403e615dc129e8f26e16e444ac
Fabian Mueller <mueller.fabian@gmail.com>
2014-06-01 12:21:35 +0200

    Added check if DOI is valid and raise ValueError if not; allowed to create empty DOIs

--- lib/doi.py
+++ lib/doi.py
@@ -1,8 +1,6 @@
 import os
 import sys
-
-class DOIError(Exception):
-    pass
+import re
 
 class DOI(object):
     """
@@ -12,16 +10,29 @@ class DOI(object):
     URL_PROTOCOL = "http"
     URL_DOMAIN   = "dx.doi.org"
 
-    def __init__(self, identifier):
-        self.identifier = self.__parse_DOI_identifier(identifier)
-        self.url = self.__generate_DOI_URL(self.identifier)
+    UNKNOWN_IDENTIFIER = 'unknown_identifier'
+
+    def __init__(self, identifier=None):
+        self.identifier = self.UNKNOWN_IDENTIFIER
+        self.url = None
+
+        if identifier is not None:
+            self.identifier = self.__parse_DOI_identifier(identifier)
+            self.url = self.__generate_DOI_URL(self.identifier)
 
     def __parse_DOI_identifier(self, identifier):
+        if not self.__is_valid_doi(identifier):
+            raise ValueError("DOI is invalid. Given DOI: \
+{}".format(identifier))
         if identifier.startswith("http"):
             #raise DOIError("DOI identifier cannot be URLs!")
             return identifier[len("{}://{}/".format(self.URL_PROTOCOL, \
                 self.URL_DOMAIN)):]
         return identifier
+
+    def __is_valid_doi(self, identifier):
+        regex = re.compile("(http\:\/\/dx\.doi\.org/)?10\.[\d\.]+(/*)?")
+        return regex.match(identifier) is not None
 
     def __generate_DOI_URL(self, identifier):
         return "{}://{}/{}".format(self.URL_PROTOCOL, self.URL_DOMAIN, \
